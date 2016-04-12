3fa2aea25039adda5d42e9c8f794bd69161e0189
Damian Krysta <krypton9208@gmail.com>
2016-04-12 08:19:14 +0200

    Start implementing OWIN with NHibernate

--- Learn.Models/LearnDbContext.cs
+++ Learn.Models/LearnDbContext.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NHibernate.AspNet.Identity;
+using SharpArch.Domain.DomainModel;
 
 namespace Learn.Models
 {
-    public class LearnUser : IdentityUser
+    public class LearnDbContext : Entity
     {
-
+        public LearnUser User { get; set; }
     }
 }
